45eec46d77aab85748e341de1ccc39728375aefd
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-03-17 17:45:33 -0400

    removing project templates

--- R/data.R
+++
@@ -1,7 +0,0 @@
-#' PCCTC project templates
-#'
-#' The `project_templates` object defines the contents of the PCCTC project
-#' templates used in `create_pcctc_project()` and `use_pcctc_file()`.
-#'
-#' @format A named list containing the PCCTC project templates.
-"project_templates"
--- inst/project-templates/readme_subdir.md
+++
@@ -1,5 +0,0 @@
-## {{folder_name}} {{getOption('croquet.name')}} 
-
-## Project Log
-**{{Sys.Date()}}**
-Record import milestones/decisions made here.
