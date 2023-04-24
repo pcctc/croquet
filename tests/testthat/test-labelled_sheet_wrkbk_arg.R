c1e99d4b3dcb4fcf85a7efe604d2bb612c8abf3f
harrism1 <harrism1@mskcc.org>
2023-04-24 16:21:28 -0400

    removed snapshots and corrected testing for labelled fxs.

--- tests/testthat/test-labelled_sheet_wrkbk_arg.R
+++ tests/testthat/test-labelled_sheet_wrkbk_arg.R
@@ -1,26 +1,37 @@
-test_that("labelled_sheet wrkbk argument works", {
+dat1 <- tibble::tibble(
+  var_1 = 1:3
+) %>%
+  labelled::set_variable_labels(
+    var_1 = "Variable 1 (numeric)",
+  )
 
-  dat1 <- tibble::tibble(
-    var_1 = 1:3
-    ) %>%
-    labelled::set_variable_labels(
-      var_1 = "Variable 1 (numeric)",
-    )
+dat2 <- dat1
 
-  dat2 <- tibble::tibble(
-    var_2 = LETTERS[1:3],
-  ) %>%
-    labelled::set_variable_labels(
-      var_2 = "Variable 2 (character)",
-    )
+test_that("labelled_sheet does not give an error when wb object exists in the calling environment", {
 
   wb <- openxlsx::createWorkbook()
 
-  # To test that missing wrkbk argument throws an error
-  expect_snapshot(labelled_sheet(data = dat1), NA)
-  expect_snapshot(add_labelled_sheet(data = dat2), NA)
+  # To test that error is not given when wb is in the environment
+  expect_error(labelled_sheet(data = dat1), NA)
+  expect_error(add_labelled_sheet(data = dat2), NA)
 
 })
 
 
+test_that("labelled_sheet throw an error when no wb object found in the calling environment", {
 
+  # To test that missing wrkbk argument throws an error
+  expect_error(labelled_sheet(data = dat1))
+  expect_error(add_labelled_sheet(data = dat2))
+
+})
+
+test_that("labelled_sheet wrkbk argument works with alternatively named workbook (not wb)", {
+
+  meghans_wb <- openxlsx::createWorkbook()
+
+  # To test that alternatively named wbs work
+  expect_error(labelled_sheet(data = dat1, wrkbk = meghans_wb), NA)
+  expect_error(add_labelled_sheet(data = dat2, wrkbk = meghans_wb), NA)
+
+})
